1a580509b69026c4c8da1bc65d862dbfd3f560f3
NgocHiep-Class <hieptcn@gmail.com>
2024-09-25 00:00:39 +0700

    BT1

---
+++ HelloJava/src/MyClass/MyClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package MyClass;
+
+/**
+ * 
+ */
+public class MyClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
